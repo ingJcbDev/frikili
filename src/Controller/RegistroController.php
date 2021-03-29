ec00eed7e6b5a20cb2133afd55dcb8d437c32014
ingJcbDev <ing.jcb.dev@gmail.com>
2021-03-28 23:03:06 -0500

    08. Curso de Symfony 5 - Encriptando contraseña

--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -8,13 +8,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistroController extends AbstractController
 {
     /**
      * @Route("/registro", name="registro")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
@@ -23,6 +24,7 @@ class RegistroController extends AbstractController
             $em = $this->getDoctrine()->getManager();
             $user->setBaneado(false);
             $user->setRoles(['ROLE_USER']);
+            $user->setPassword($passwordEncoder->encodePassword($user, $form['password']->getData()));
             $em->persist($user);
             $em->flush();
             $this->addFlash('exito', 'Se a registrado exitosamente');
